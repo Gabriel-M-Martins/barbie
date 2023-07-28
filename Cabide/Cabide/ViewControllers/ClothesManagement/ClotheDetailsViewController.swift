672bfb96c2f7ba588a7bc003dd6d5051972636e3
Luana Thomas <luana.thomas@hotmail.com>
2023-07-28 16:39:20 -0300

    Initial interface - create and list clothes

---
+++ Cabide/Cabide/ViewControllers/ClothesManagement/ClotheDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ClotheDetailsViewController.swift
+//  Cabide
+//
+//  Created by Luana Tais Thomas on 28/07/23.
+//
+
+import UIKit
+
+class ClotheDetailsViewController: UIViewController {
+
+    @IBOutlet weak var clotheImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var nameTextfield: UITextField!
+    @IBOutlet weak var descriptionTextfield: UITextField!
+    
+    @IBAction func updatePressed(_ sender: Any) {
+    }
+    
+    @IBAction func removePressed(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
