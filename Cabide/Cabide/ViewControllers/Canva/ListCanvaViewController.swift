419daf46833d0a31379ecf62fb02639dcdbaa818
Gabriel-M-Martins <gabrielmmartinscontato@gmail.com>
2023-08-01 19:25:58 -0300

    implemented placeholder openedcanva (only delete func)

--- Cabide/Cabide/ViewControllers/Canva/ListCanvaViewController.swift
+++ Cabide/Cabide/ViewControllers/Canva/ListCanvaViewController.swift
@@ -15,6 +15,7 @@ class ListCanvaViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         table.dataSource = self
+        table.delegate = self
     }
 }
 
@@ -33,5 +34,21 @@ extension ListCanvaViewController : UITableViewDataSource {
         cell.detailTextLabel?.text = canva.desc
         cell.imageView?.image = UIImage(data: imageData)
         return cell
+    }
+}
+
+extension ListCanvaViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let canva = viewModel.canvas[indexPath.row]
+        performSegue(withIdentifier: "toOpenedCanva", sender: canva)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toOpenedCanva" {
+            guard let vc = segue.destination as? OpenedCanvaViewController,
+                  let info = sender as? Canva else { return }
+            
+            vc.canva = info
+        }
     }
 }
--- Cabide/Cabide/ViewControllers/Canva/OpenedCanvaViewController.swift
+++ Cabide/Cabide/ViewControllers/Canva/OpenedCanvaViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class OpenedCanvaViewController: UIViewController {
 
-    @IBOutlet weak var container: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var thumbnail: UIImageView!
+    
+    var canva: Canva?
+    var viewModel: OpenedCanvaViewModel = OpenedCanvaViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let canva = self.canva else { return }
+        nameLabel.text = canva.name
+        descriptionLabel.text = canva.desc
+        
+        let imageData = canva.thumbnail ?? Data()
+        thumbnail.image = UIImage(data: imageData)
     }
+    
+    @IBAction func deleteCanva(_ sender: Any) {
+        guard let canva = self.canva else { return }
+        viewModel.service.delete(canva)
+    }
+    
 }
