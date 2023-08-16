34b74d5e747ec5bb3ae7ed35a722a16fbe55ceb9
Gabriel-M-Martins <gabrielmmartinscontato@gmail.com>
2023-08-16 15:17:14 -0300

    added tags to createClothe view

--- Cabide/Cabide/ViewControllers/ClothesManagement/CreateClotheViewController.swift
+++ Cabide/Cabide/ViewControllers/ClothesManagement/CreateClotheViewController.swift
@@ -10,17 +10,39 @@ import PhotosUI
 
 class CreateClotheViewController: UIViewController, PHPickerViewControllerDelegate, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     
-    let viewModel = ClotheViewModel()
+    let model = ClotheViewModel()
     weak var delegate: CreateClotheDelegate?
 
     @IBOutlet weak var clotheImage: UIImageView!
     @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var tagsCollection: UICollectionView!
     @IBOutlet weak var loading: UIActivityIndicatorView!
+    
+    let filterCell = UINib(nibName: "FilterCollectionViewCell", bundle: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         saveButton.isEnabled = false
         loading.isHidden = true
+        
+        tagsCollection.delegate = self
+        tagsCollection.dataSource = self
+        tagsCollection.register(filterCell, forCellWithReuseIdentifier: "FilterCell")
+        
+        clotheImage.layer.shadowColor = UIColor.lightGray.cgColor
+        clotheImage.layer.shadowOffset = CGSize(width: 0, height: 1)
+        clotheImage.layer.shadowOpacity = 0.8
+        clotheImage.layer.shadowRadius = 2.0
+        clotheImage.clipsToBounds = false
+        
+        let layout = LeftAlignedCollectionViewFlowLayout()
+        layout.minimumLineSpacing = 10.0
+        layout.minimumInteritemSpacing = 10.0
+        
+        tagsCollection.collectionViewLayout = layout
+        if let filtersCollectionViewLayout = tagsCollection.collectionViewLayout as? UICollectionViewFlowLayout {
+            filtersCollectionViewLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        }
         
         let cancelButton = UIBarButtonItem(title: "Cancelar", style: .plain, target: self, action: #selector(cancelPressed) )
         cancelButton.tintColor = UIColor(named: "Roxo") ?? UIColor.black
@@ -54,7 +76,7 @@ class CreateClotheViewController: UIViewController, PHPickerViewControllerDelega
     }
     
     @IBAction func saveButtonPressed(_ sender: Any) {
-        viewModel.createClothe(image: clotheImage.image ?? UIImage())
+        model.createClothe(image: clotheImage.image ?? UIImage())
         delegate?.didUpdateData()
         self.presentingViewController?.dismiss(animated: true)
     }
@@ -112,4 +134,32 @@ extension CreateClotheViewController {
 
 protocol CreateClotheDelegate: AnyObject {
     func didUpdateData()
+}
+
+
+extension CreateClotheViewController : UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        model.tags.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FilterCell", for: indexPath) as? FilterCollectionViewCell
+        
+        let tag = model.tags[indexPath.row]
+        
+        cell?.name.text = tag.name
+        cell?.toggle(model.selectedTags.contains(tag))
+        
+        return cell ?? UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let tag = model.tags[indexPath.row]
+        model.toggleTag(tag)
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.tagsCollection.reloadItems(at: [indexPath])
+        }
+    }
 }
